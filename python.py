ffd6ed96517de41aa3c95c0ef79cdc92febd150f
natalia gomes <naty.pinheiro95@gmail.com>
2019-07-17 18:12:57 -0300

    jogarpython1

---
+++ python.py
@@ -0,0 +1,16 @@
+emprestimo_consorcio = ('idade,valor,renda') 
+idade = int (input('Digite a sua Idade'))
+renda = int (input('Digite a sua Renda'))
+valor_doemprestimo = int (input('Digite o valor do emprestimo'))
+     
+if (idade>=22) and (renda>1000) and (valor_doemprestimo == 2000 and 15*renda):
+    print ('Empretimo Aprovado')
+    
+else: 
+    print ('Emprestimo negado')
+ 
+ 
+ 
+# lista=['abacate', 'limão', 'amora']
+#   for item in lista
+#  print 
